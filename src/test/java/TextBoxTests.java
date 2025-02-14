a249126b7c1df1d8a2d7a5755cfbda13b3b73f90
Pushkarev.EE <lvstcentury@gmail.com>
2025-02-14 09:20:54 +0700

    add output checks

--- src/test/java/TextBoxTests.java
+++ src/test/java/TextBoxTests.java
@@ -1,24 +1,41 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class TextBoxTests {
-    @Test
-    void fillFormTest() {
+
+    @BeforeAll
+    static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
+    }
+
+    String userName = "Andrey";
+    String userEmail = "abc123@lookout.com";
+    String currentAddress = "Москва, ул. Виноградная";
+    String permanentAddress = "Moscow, Arbatskaya street";
+
+    @Test
+    void fillFormTest() {
 
         open("/text-box");
-        $("#userName").setValue("Andrey");
-        $("#userEmail").setValue("abc123@lookout.com");
-        $("#currentAddress").setValue("Москва, ул. Виноградная");
-        $("#permanentAddress").setValue("Санкт-Петербург, ул. Московская");
+        $("#userName").setValue(userName);
+        $("#userEmail").setValue(userEmail);
+        $("#currentAddress").setValue(currentAddress);
+        $("#permanentAddress").setValue(permanentAddress);
         $("#submit").click();
         $("#output").shouldBe(visible);
+
+        $("#output #name").shouldHave(text(userName));
+        $("#output #email").shouldHave(text(userEmail));
+        $("#output #currentAddress").shouldHave(text(currentAddress));
+        $("#output #permanentAddress").shouldHave(text(permanentAddress));
 
     }
 }

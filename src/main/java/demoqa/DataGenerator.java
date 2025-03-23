78fb911f230f61fb6a8db8b1f73eb2437382cec0
Pushkarev.EE <lvstcentury@gmail.com>
2025-03-23 21:03:22 +0700

    add DataGenerator add interacts with elements on page

---
+++ src/main/java/demoqa/DataGenerator.java
@@ -0,0 +1,45 @@
+package demoqa;
+
+import java.time.Year;
+import java.util.Random;
+
+public class DataGenerator {
+
+    public String randomPhoneNumber(int length) {
+        Random randomPhone = new Random();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < length; i++) {
+            sb.append(randomPhone.nextInt(10)); // Generates a random digit (0-9)
+        }
+
+        return sb.toString();
+    }
+
+    public String RandomDate() {
+        Random randomDate = new Random();
+        int currentYear = Year.now().getValue();
+        int maxAgeForLoan = 20;
+        int year = randomDate.nextInt(currentYear - maxAgeForLoan + 1) + 65; // Random year
+        int month = randomDate.nextInt(12) + 1; // Random month between 1 and 12
+        int day = switch (month) {
+            case 2 -> // February
+                    randomDate.nextInt(isLeapYear(year) ? 29 : 28) + 1;
+            case 4, 6, 9, 11 -> // Months with 30 days
+                    randomDate.nextInt(30) + 1;
+            default -> // Months with 31 days
+                    randomDate.nextInt(31) + 1;
+        };
+
+        // Determine the maximum days in the given month
+
+        // Format the date
+        return String.format("%02d-%02d-%d", day, month, year);
+    }
+
+    private static boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+}
+
+
--- src/test/java/TextBoxTests.java
+++ src/test/java/TextBoxTests.java
@@ -1,41 +1,50 @@
 import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.AfterAll;
+import demoqa.DataGenerator;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
 
 public class TextBoxTests {
 
+    DataGenerator dataGenerator = new DataGenerator();
+
     @BeforeAll
+
     static void beforeAll() {
-        Configuration.browserSize = "1920x1080";
+        Configuration.browserSize = "425x960";
         Configuration.baseUrl = "https://demoqa.com";
     }
+
 
     String userName = "Andrey";
     String userEmail = "abc123@lookout.com";
     String currentAddress = "Москва, ул. Виноградная";
-    String permanentAddress = "Moscow, Arbatskaya street";
+    String phoneNumber = dataGenerator.randomPhoneNumber(10);
 
     @Test
     void fillFormTest() {
 
-        open("/text-box");
-        $("#userName").setValue(userName);
+        open("/automation-practice-form");
+        $("#firstName").setValue(userName);
+        $("#lastName").setValue(userName);
         $("#userEmail").setValue(userEmail);
+        $("#gender-radio-1").click();
+        $("#userNumber").setValue(phoneNumber);
+        $("#dateOfBirthInput").clear();
+        $("#dateOfBirthInput").setValue(dataGenerator.RandomDate());
+        $("#hobbies-checkbox-1").click();
+        $("#hobbies-checkbox-2").click();
+        $("#hobbies-checkbox-3").click();
+        $("#state").selectOptionByValue("NCR");
+        $("#city").selectOption(0);
         $("#currentAddress").setValue(currentAddress);
-        $("#permanentAddress").setValue(permanentAddress);
         $("#submit").click();
         $("#output").shouldBe(visible);
 
-        $("#output #name").shouldHave(text(userName));
-        $("#output #email").shouldHave(text(userEmail));
-        $("#output #currentAddress").shouldHave(text(currentAddress));
-        $("#output #permanentAddress").shouldHave(text(permanentAddress));
+        $(".modal-content").shouldBe(visible);
+        $(".modal-header").shouldHave(text("Thanks for submitting the form"));
 
     }
 }
